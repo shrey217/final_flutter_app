14baa3b07ac107080d94ca8506446d2b560ecedb
shreyanv <shreyanv@gmail.com>
2020-07-29 19:07:46 -0400

    Start Page code Problem with child/children

--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,60 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
   home: Scaffold(
+    backgroundColor: Colors.greenAccent,
     body: Center(
-        child:
-          Column(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'Welcome to',
-                  style: TextStyle(
-                      fontSize: 30.0
-                  ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Welcome to ',
+                style: TextStyle(
+                  fontSize: 30.0
                 ),
-              ])
-        ),
-
-    )
-
-)
+              ),
+          Row(
+            children: <Widget>[
+              Text(
+                'Ka-Ching',
+                style: TextStyle(
+                  fontSize: 30.0
+                )
+                  )
+            ],
+              ),
+            ],
+          ),
+          FlatButton(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(18.0),
+              side: BorderSide(color: Colors.blue)),
+            color: Colors.red,
+            textColor: Colors.white,
+            padding: EdgeInsets.all(8.0),
+            onPressed: () {  },
+            child: Text(
+              'Login',
+              style: TextStyle(
+                fontWeight: FontWeight.bold
+              ),
+              ) ,
+            ),
+          FlatButton(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(18.0),
+                side: BorderSide(color: Colors.blue)),
+            color: Colors.red,
+            textColor: Colors.white,
+            padding: EdgeInsets.all(8.0),
+            onPressed: () {  },
+            child: Text(
+              'Sign Up',
+            ) ,
+          ),
+          ]),
+              ),
+            ),
 ));
